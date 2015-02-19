2f98a5ef7169a4459327ff83784b47a8134b0557
Jorge Cano <jorgeucano@gmail.com>
2015-02-19 14:56:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install git nodejs npm
-git clone https://github.com/jonatanariste/GDGNodeBoilerPlate.git
+git clone https://github.com/jorgeucano/GDGNodeBoilerPlate.git
 cd GDGNodeBoilerPlate/
 npm install
 vi start.sh
